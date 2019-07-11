ef7e276b81190820b86eeb0c97b8896c7afa34ae
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-07-11 18:52:28 +0200

    Creating the T shaped element

--- tetris.js
+++ tetris.js
@@ -5,3 +5,6 @@ const context = canvas.getContext("2d");
 // Drawing on canvas
 context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+// Creating T shaped piece
+const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
