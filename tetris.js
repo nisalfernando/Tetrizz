4aeae26497e2e51a1487411a20ec858b0bc56693
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-07-11 18:47:23 +0200

    Testing the canvas

--- tetris.js
+++ tetris.js
@@ -1,3 +1,7 @@
 // Accessing the canvas
 const canvas = document.querySelector("#tetris");
 const context = canvas.getContext("2d");
+
+// Drawing on canvas
+context.fillStyle = "#000";
+context.fillRect(0, 0, canvas.clientWidth, canvas.height);
