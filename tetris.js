bcd91182a0e3c28bf9ba2ae0ef7b69a7dbdf4556
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-07-13 20:24:21 +0200

    Making the pieces move for later use

--- tetris.js
+++ tetris.js
@@ -13,15 +13,15 @@ context.fillRect(0, 0, canvas.clientWidth, canvas.height);
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
 // Drawing the T shaped piece
-function drawMatrix(matrix) {
+function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
                 context.fillStyle = "red";
-                context.fillRect(x, y, 1, 1);
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
     });
 }
 
-drawMatrix(matrix);
+drawMatrix(matrix, { x: 5, y: 5 });
