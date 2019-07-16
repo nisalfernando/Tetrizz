1cd0cf2c34aad202f474866b247b78f11a022d48
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-07-16 20:41:36 +0200

    Created a function for down arrow key

--- tetris.js
+++ tetris.js
@@ -28,6 +28,12 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+// For the down arrow key
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -63,8 +69,7 @@ document.addEventListener("keydown", event => {
     } else if (event.keyCode === 39) {
         player.pos.x++;
     } else if (event.keyCode === 40) {
-        player.pos.y++;
-        dropCounter = 0;
+        playerDrop();
     }
 });
 
