dad296a56acb22925bd5018c70ef1b74d1294804
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-07-20 20:34:41 +0200

    Modified the rotate function

--- tetris.js
+++ tetris.js
@@ -87,7 +87,9 @@ function playerMove(dir) {
 
 function rotete(matrix, dir) {
     for (let y = 0; y < matrix.length; ++x) {
-        for (let x = 0; x < y; ++x) {}
+        for (let x = 0; x < y; ++x) {
+            [matirx[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+        }
     }
 }
 
